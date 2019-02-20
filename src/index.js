8532074595508c7a7b9be33c1bb6a1602c681bf9
GeniusK <geniusk@rinc.kr>
2019-02-20 11:34:04 +0900

    FilePond 써보기

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+// Import React FilePond
+import { FilePond, File, registerPlugin } from 'react-filepond'
 
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css'
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 import './styles.css'
 
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+
 function App() {
+  const [files, setFiles] = useState([])
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <FilePond
+        allowMultiple={false}
+        onupdatefiles={setFiles}
+        labelIdle='2MB 이하의 png, gif, jpg(jpeg), pdf, tif(tiff) <span class="filepond--label-action">파일 찾기</span>'
+      >
+        {files.map(fileItem => {
+          return <File key={fileItem.file} src={fileItem.file} origin="local" />
+        })}
+      </FilePond>
     </div>
   )
 }
